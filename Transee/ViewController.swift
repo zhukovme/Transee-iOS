d22c5bd5c05800265c0061788c2a0fa24230d02d
zhukovme <zhukovme@gmail.com>
2015-06-08 21:07:58 +0300

    Initial Commit

---
+++ Transee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transee
+//
+//  Created by Michael on 08.06.15.
+//  Copyright (c) 2015 Transee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
